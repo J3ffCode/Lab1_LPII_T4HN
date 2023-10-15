cf100954c60c90a5c72b8fe020c0d62b8127e3d4
JefferLP02 <I202223377@cibertec.edu.pe>
2023-10-15 03:45:01 -0500

    Se agrego mi primer git del trabajo T1 del curso LP2

---
+++ src/main/java/views/EmpleadoView.java
@@ -0,0 +1,27 @@
+package views;
+import controllers.EmpleadoController;
+import models.Empleado;
+
+
+public class EmpleadoView {
+	public static void main(String[] args) {
+		
+				//Create
+				//String empleado = new EmpleadoController().createEmpleado("Solorzano", "Melissa", 30, "F", 3000);
+				String empleado = new EmpleadoController().createEmpleado("Ortiz de Orue", "Jeffer", 32, "M", 6000);
+				System.out.println(empleado);
+				//String empleado = new EmpleadoController().createEmpleado("Zavaleta", "Pancho", 3, "M", 1000);
+				
+				//Read
+				//Empleado empleado1 = new EmpleadoController().readerEmpleado(2);
+				//System.out.println(empleado1);
+				
+				//Delete
+				//String empleado = new EmpleadoController().deleteEmpleado(2);
+				
+				
+				//Update
+				//String empleado = new EmpleadoController().updateEmpleado(1,"Ortiz Zavaleta", "Beto", 4, "M", 800);
+	}
+
+}
